eb4e02890e755031d02b58880605907e29153366
IK4MS <ikill4myskill@outlook.com>
2023-08-18 19:34:51 +0200

    Update pre-install-all.ps1 (not sure it's necessary I wrote this months ago)

--- src/cleanup-desktop-shortcuts.hook/hook/pre-install-all.ps1
+++ src/cleanup-desktop-shortcuts.hook/hook/pre-install-all.ps1
@@ -1,4 +1,7 @@
-﻿if (-not $packageScript) {
+#Let's remove those damn taskbar pins too (and god knows if this even works in different versions, there's like five different ways to unpin something from taskbar)
+$quicklaunch = Join-Path ([environment]::GetFolderPath("ApplicationData")) 'Microsoft\Internet Explorer\Quick Launch'
+
+if (-not $packageScript) {
     Write-Host "Skipping Cleanup of Desktop Shortcuts as there is no install script for this package"
     return
 }
@@ -9,7 +12,7 @@ if ([string]::IsNullOrEmpty([Environment]::GetFolderPath("DesktopDirectory"))) {
 }
 
 $global:CleanupDesktopShortcutsHook = @{
-    PreShortcuts = @(Get-Childitem -Path ([environment]::GetFolderPath("DesktopDirectory")) -Filter "*.lnk")
+    PreShortcuts = @(Get-Childitem -Path $quicklaunch,([environment]::GetFolderPath("DesktopDirectory")) -Filter "*.lnk" -Recurse -Force)
 }
 
 if (Test-ProcessAdminRights) {
